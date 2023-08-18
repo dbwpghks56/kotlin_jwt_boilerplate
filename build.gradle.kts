7eb5b9252907068e5d072d0a0420825882737e9a
wpghks <wpghks56@naver.com>
2023-08-18 09:45:56 +0900

    [init] project Init

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,24 @@ plugins {
 	war
 	id("org.springframework.boot") version "2.7.15-SNAPSHOT"
 	id("io.spring.dependency-management") version "1.0.15.RELEASE"
+	id("org.jetbrains.kotlin.plugin.allopen") version "1.6.21"
+	kotlin("kapt") version "1.8.21"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 	kotlin("plugin.jpa") version "1.6.21"
+}
+
+// QueryDSL 설정부
+sourceSets["main"].withConvention(org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet::class) {
+	kotlin.srcDir("$buildDir/generated/source/kapt/main")
+}
+//
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+	annotation("com.chat.socket.Annotation")
 }
 
 group = "com.jwt"
@@ -29,6 +44,10 @@ repositories {
 }
 
 dependencies {
+	// swagger 설정
+	implementation ("io.springfox:springfox-boot-starter:3.0.0")
+	implementation ("io.springfox:springfox-swagger-ui:3.0.0")
+	//
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
@@ -43,6 +62,10 @@ dependencies {
 	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	// QueryDsl 설정
+	implementation("com.querydsl:querydsl-jpa:5.0.0")
+	kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+	//
 }
 
 tasks.withType<KotlinCompile> {
